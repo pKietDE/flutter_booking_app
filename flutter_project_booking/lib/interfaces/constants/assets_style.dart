e2bea46c155e48b86170019870059d6b7ecfd820
pKietDE <phanmaiquockiet2004@gmail.com>
2024-12-26 18:07:09 +0700

    Optimized project and create pages : introduce, signin , signup ...

---
+++ flutter_project_booking/lib/interfaces/constants/assets_style.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class AssetStyle {
+  // Heading styles
+  static const TextStyle h1 = TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSerif',
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSerif',
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'NotoSerif',
+  );
+
+  static const TextStyle h4 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSerif',
+  );
+
+  static const TextStyle subtitle1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSans',
+  );
+
+  static const TextStyle body1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSans',
+  );
+
+  static const TextStyle subtitle2 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSans',
+  );
+
+  static const TextStyle body2 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSans',
+  );
+
+  static const TextStyle button = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSans',
+  );
+
+  static const TextStyle actionDialog = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'NotoSerif',
+  );
+}
---
+++ flutter_project_booking/lib/pages/login/sign_up.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../../interfaces/constants/assets_image.dart';
+import '../../interfaces/constants/assets_icon.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        color: Colors.amber,
+                        child: Image.asset(
+                          AssetImages.singupImage,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                          top: 150,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                AssetIcon.icBrandingB,
+                                height: 55,
+                              ),
+                              Image.asset(
+                                AssetIcon.icBookingcom,
+                                height: 35,
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
